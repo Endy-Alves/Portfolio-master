45ccf19663ed006e8dd2d8bacc2548325ee67dad
Endy Alves <alvesandy32@gmail.com>
2023-03-15 16:13:15 -0300

    responsividade

---
+++ main.js
@@ -0,0 +1,14 @@
+const myImage = document.getElementsByClassName(".image-profile");
+const divDaImagem = document.createElement("div");
+const texto = document.createElement("img");
+divDaImagem.appendChild(texto);
+console.log(divDaImagem);
+
+new Image("./images/IMG-20220909-WA0051.jpg");
+
+myImage.onclick = clicarNaImagem;
+function clicarNaImagem(){
+
+}
+
+console.log(imagemGrande);
